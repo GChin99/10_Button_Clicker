e6c0e1df4466dda05d5b37d1361f2f47d7f296f9
Gchin99 <gchin99@gmail.com>
2022-07-17 17:43:03 -0700

    first draft

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("javascript is here, DOM loaded!")
+
+function changeText(element) {
+    element.innerText = "Logout"
+}
+
+function alert_(element) {
+    alert("Ninja was liked!")
+}
+
+function removeButton(element) {
+    element.remove()
+}
